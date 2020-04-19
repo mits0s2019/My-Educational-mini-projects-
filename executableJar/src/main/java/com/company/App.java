37b96d177112d008041aea07e1bf8004a4794e76
mits0s2019 <mits0s200efta@gmail.com>
2020-04-19 21:18:46 +0300

    executable jar

---
+++ executableJar/src/main/java/com/company/App.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("this is an executable jar");
+    }
+}
